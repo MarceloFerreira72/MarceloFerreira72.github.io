16fcadfecbda8a3e69ba4aa694defa0c4bfec4f3
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2023-02-22 23:11:40 -0300

    estrutura inicial

---
+++ meu-portfolio/src/Components/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutMe extends React.Component {
+  render() {  
+    return (
+      <main>
+        <h1>Portfólio - Marcelo Ferreira</h1>
+        <img id="minha foto" src="../"/>
+      </main>
+    )
+  }
+}
+
+export default AboutMe;
