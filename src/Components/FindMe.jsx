832437ea425eeed8138164988fc4d2962f493ee5
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2023-04-04 11:40:54 -0300

    refactor: iguala tamanho de ícones de contato

--- src/Components/FindMe.jsx
+++ src/Components/FindMe.jsx
@@ -10,7 +10,7 @@ function FindMe() {
           <img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' alt='github icon' width='50px' />
         </Link>
         <Link to='https://www.linkedin.com/in/devmarceloferreira/' target='_blank'>
-          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png' alt='linkedin icon' width='40px'/>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png' alt='linkedin icon' width='50px'/>
         </Link>
       </div>
     </header>
